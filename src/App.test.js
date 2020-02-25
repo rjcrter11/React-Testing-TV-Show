3c3379a87ae4e40807feea8fcf4971df6b2ad745
rjcrter11 <rjcrter11@gmail.com>
2020-02-24 19:21:09 -0600

    got a couple working. I dont know what im doing

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { render, fireEvent, wait } from "@testing-library/react";
+import { render, wait, getByText } from "@testing-library/react";
 import { fetchShow as mockFetchShow } from "./api/fetchShow";
-
 import App from "./App";
 import Dropdown from "react-dropdown";
-
-// jest.mock("./api/fetchShow");
+import userEvent from "@testing-library/user-event";
 
 const shows = {
   data: {
@@ -506,213 +504,64 @@ const shows = {
               href: "http://api.tvmaze.com/episodes/1576469"
             }
           }
-        },
-        {
-          id: 1576470,
-          url:
-            "http://www.tvmaze.com/episodes/1576470/stranger-things-3x02-chapter-two-the-mall-rats",
-          name: "Chapter Two: The Mall Rats",
-          season: 3,
-          number: 2,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 50,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/511261.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/511261.jpg"
-          },
-          summary:
-            "<p>While El and Max go shopping, Nancy and Wheeler follow up on the exploding rats. Billy takes a coworker on a field trip, and Joyce blows off dinner with Jim to meet with Mr. Clarke.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576470"
-            }
-          }
-        },
-        {
-          id: 1576471,
-          url:
-            "http://www.tvmaze.com/episodes/1576471/stranger-things-3x03-chapter-three-the-case-of-the-missing-lifeguard",
-          name: "Chapter Three: The Case of the Missing Lifeguard",
-          season: 3,
-          number: 3,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 50,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/511286.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/511286.jpg"
-          },
-          summary:
-            "<p>El goes astrally projecting and discovers that Billy has done something with Heather. While the girls try to find the missing lifeguard, Will tries to get Mike and Lucas interested in D&amp;D. Dustin and Steve spy on the mall shops, and Robin tries to decipher the Russian message.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576471"
-            }
-          }
-        },
-        {
-          id: 1576472,
-          url:
-            "http://www.tvmaze.com/episodes/1576472/stranger-things-3x04-chapter-four-the-sauna-test",
-          name: "Chapter Four: The Sauna Test",
-          season: 3,
-          number: 4,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 53,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/511312.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/511312.jpg"
-          },
-          summary:
-            "<p>Mike, Lucas, and Will recruit El and Max to help them learn who the Mind Flayer's host is. Robin, Steve, and Dustin recruit someone to sneak into Lynx, and Nancy and Jonathan are fired.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576472"
-            }
-          }
-        },
-        {
-          id: 1576473,
-          url:
-            "http://www.tvmaze.com/episodes/1576473/stranger-things-3x05-chapter-five-the-flayed",
-          name: "Chapter Five: The Flayed",
-          season: 3,
-          number: 5,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 52,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/510102.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/510102.jpg"
-          },
-          summary:
-            "<p>The Scoop Group explore the tunnels beneath Starcourt, while Nancy and Jonathan recruit Mike and the others to help them discover what happen to the Holloways and find the Mind Flayer's base of operations.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576473"
-            }
-          }
-        },
-        {
-          id: 1576474,
-          url:
-            "http://www.tvmaze.com/episodes/1576474/stranger-things-3x06-chapter-six-e-pluribus-unum",
-          name: "Chapter Six: E Pluribus Unum",
-          season: 3,
-          number: 6,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 60,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/511681.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/511681.jpg"
-          },
-          summary:
-            "<p>Dr. Alexei reveals what the Russians have been building, and Eleven sees where Billy has been. Dustin and Erica stage a daring rescue.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576474"
-            }
-          }
-        },
-        {
-          id: 1576475,
-          url:
-            "http://www.tvmaze.com/episodes/1576475/stranger-things-3x07-chapter-seven-the-bite",
-          name: "Chapter Seven: The Bite",
-          season: 3,
-          number: 7,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 55,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/511708.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/511708.jpg"
-          },
-          summary:
-            "<p>El and the others defend themselves as the Flayer tracks them down. The Scoop Group escape the Russian base and hide in a movie theater, but the Russians close in on them.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576475"
-            }
-          }
-        },
-        {
-          id: 1576476,
-          url:
-            "http://www.tvmaze.com/episodes/1576476/stranger-things-3x08-chapter-eight-the-battle-of-starcourt",
-          name: "Chapter Eight: The Battle of Starcourt",
-          season: 3,
-          number: 8,
-          airdate: "2019-07-04",
-          airtime: "",
-          airstamp: "2019-07-04T12:00:00+00:00",
-          runtime: 78,
-          image: {
-            medium:
-              "http://static.tvmaze.com/uploads/images/medium_landscape/204/510105.jpg",
-            original:
-              "http://static.tvmaze.com/uploads/images/original_untouched/204/510105.jpg"
-          },
-          summary:
-            "<p>The Flayer comes to the mall to get El, and Dustin contacts Suzie to provide Jim and Joyce with needed information. El reminds Billy of his past, and Jim and Joyce have an argument about not arguing.</p>",
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1576476"
-            }
-          }
-        },
-        {
-          id: 1752754,
-          url:
-            "http://www.tvmaze.com/episodes/1752754/stranger-things-4x01-chapter-one-the-hellfire-club",
-          name: "Chapter One: The Hellfire Club",
-          season: 4,
-          number: 1,
-          airdate: "",
-          airtime: "",
-          airstamp: null,
-          runtime: 60,
-          image: null,
-          summary: null,
-          _links: {
-            self: {
-              href: "http://api.tvmaze.com/episodes/1752754"
-            }
-          }
         }
       ]
     }
   }
 };
 
-test("App component renders correctly", () => {
-  const { queryByText } = render(<App />);
+// test("App component renders correctly", () => {
+//   const { queryByText } = render(<App />);
 
-  expect(queryByText(/fetching data/i)).not.toBeNull();
-});
+//   // expect(queryByText(/fetching data/i)).not.toBeNull();
+// });
 
-test("i dunno", () => {
-  const { queryByTestId } = render(<Dropdown />);
+test("dropdown renders", () => {
+  const { queryByTestId, queryByText } = render(<Dropdown />);
   queryByTestId(/react-dropdown/i);
+  expect(queryByText(/fetching data/i)).toBeNull();
 });
+
+jest.mock("./api/fetchShow");
+
+test("App renders the show data", async () => {
+  mockFetchShow.mockResolvedValueOnce(shows);
+});
+
+test("Dropdown renders", async () => {
+  const { getByText } = render(<App />);
+
+  await wait();
+
+  userEvent.click(getByText(/select a season/i));
+
+  // const seasonOne = getByText(/season 1/i);
+
+  wait(() => {
+    // userEvent(seasonOne);
+    expect(getByText(/season 1/i)).toHaveLength(8);
+
+    const seasonOne = userEvent.click(getByText(/season 1/i));
+    const seasonTwo = userEvent.click(getByText(/season 2/i));
+    const seasonThree = userEvent.click(getByText(/season 3/i));
+    const seasonFour = userEvent.click(getByText(/season 4/i));
+    expect(seasonOne).toHaveLength(1);
+    expect(seasonTwo).toHaveLength(1);
+    expect(seasonThree).toHaveLength(1);
+    expect(seasonFour).toHaveLength(2);
+  });
+});
+
+// test("Each selection renders", async () => {
+//   const { getByText } = render(<App />);
+
+//   await wait();
+
+//   userEvent.click(getByText(/select a season/i));
+//   const seasonOne = getByText(/season 1/i);
+
+//   wait(() => {
+//     userEvent(seasonOne);
+//     expect(getByText(/season 1/i)).toHaveLength(1);
+//   });
+// });
